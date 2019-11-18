23ec10ac8632750d695f47cd36c7ef9eaf4c2d64
Yon Mardianto <yon@mackbook-air-yon.local>
2019-11-18 10:49:47 +0700

    Updates

---
+++ precache-manifest.522ba52c1a33a3f0c824b522a29fa347.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd05535660489b031e772cc503fe5ce",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6918354eb231e878d1b5",
+    "url": "/monsters-rolodex/static/css/main.f5cb3f0b.chunk.css"
+  },
+  {
+    "revision": "ef416eb161882e7a9b25",
+    "url": "/monsters-rolodex/static/js/2.c844611d.chunk.js"
+  },
+  {
+    "revision": "6918354eb231e878d1b5",
+    "url": "/monsters-rolodex/static/js/main.709a737f.chunk.js"
+  },
+  {
+    "revision": "fd56c18d112aedca6c08",
+    "url": "/monsters-rolodex/static/js/runtime~main.837baca9.js"
+  }
+]);
